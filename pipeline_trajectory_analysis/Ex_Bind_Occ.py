81e077089d4b54986c40a5af7261ef0a0fe73d51
mallu2 <malin.lueking@gmx.com>
2022-11-22 08:48:05 +0100

    updated script to new reference format

--- pipeline_trajectory_analysis/Ex_Bind_Occ.py
+++ pipeline_trajectory_analysis/Ex_Bind_Occ.py
@@ -16,12 +16,16 @@ from Spiral import plot
 from Spiral import Region
 from Spiral import Iterator_full as Iterator
 
-topo_N = '/home_d/malin/scripts/AnalysisPy/Binding/ADNAex.dat'
-targetC = '/home_d/malin/scripts/AnalysisPy/Binding/fullLength.txt'
+
+#Specifiy the location of the DNA topology (ADNAex.dat or BDNAex.dat)
+topo_N = 'input_contact_analysis/ADNAex.dat'
+#Specifiy the location of the target site reference"
+targetC = 'input_contact_analysis/fullLength.txt'
 
 class P:
     def readlinesfromfile(file):
         """Returns list of lines in the file."""
+
         lines = []
         with open(file, "r") as f:
             lines.append(f.readlines())
@@ -29,6 +33,7 @@ class P:
     
     def P_coordinates(DNA):
         """Get the coordinates for the P in the DNA in float form."""
+
         DNA_lines = P.readlinesfromfile(DNA)
         P_lines_C = [line for line in DNA_lines[0] if 'P    C' in line]
         P_lines_G = [line for line in DNA_lines[0] if 'P    G' in line]
@@ -52,68 +57,9 @@ class P:
         
         return(dfP)
 
-class analyse_traj:
-    
-    def COM(data):
-        """Get the Coordinates and do the an operation on it."""
-        COMl = []
-        for frame in data:
-            COMl.append(COoperations.COM(Coordinates.get_coord_traj(frame))) 
-        return(COMl)
-    
-    def CZ(data):
-        """Get the Coordinates and do the an operation on it."""
-        CZl= []
-        for frame in data:
-             CZl.append(COoperations.CZ(Coordinates.get_coord_traj(frame))) 
-        return(CZl)
-    
-    def CXY(data):
-        """Get the Coordinates and do the an operation on it."""
-        CXYl = []
-        for frame in data:
-            CXYl.append(COoperations.CXY(Coordinates.get_coord_traj(frame))) 
-        return(CXYl)
-    
-    def COXY(data):
-        """Get the Coordinates and do the an operation on it."""
-        COXYl = []
-        for frame in data:
-            COXYl.append(COoperations.COXY(Coordinates.get_coord_traj(frame))) 
-        return(COXYl)
-
-    def CXYCA(data):
-        """Get the Coordinates and do the an operation on it."""
-        CXYCAl = []
-        for frame in data:
-            CXYCAl.append(COoperations.CXYCA(Coordinates.get_coord_traj(frame))) 
-        return(CXYCAl)
-    
-           
-    def Displacement(folder,framecount, step):
-        """Get path of all trajectory files in folder and return the Coordinates for all atoms and different regions."""
-
-        Trajs = glob.glob(folder+'Traj*.dat')
-        Trajs.sort()
-        
-        iterator = Iterator.traj
-        
-        for Traj in Trajs:
-            CZ = analyse_traj.CZ(iterator(Traj,framecount,step))
-            CXY = analyse_traj.CXY(iterator(Traj,framecount,step))
-            COXY = analyse_traj.COXY(iterator(Traj,framecount,step))
-            ang = [] 
-            v0 = COXY[0]
-            for i in range(len(COXY)):
-                 ang.append(geom.Angle(COXY[i],v0))
-                 uang = np.unwrap(ang) 
-            f3 = open('{}'.format(Traj)+'Displacement.txt','w')
-            for i in range(len(COXY)):
-                f3.write('{l:8.3f}  {m:8.3f}  {n:8.3f} \n'.format(l = CZ[i],m=CXY[i],n=uang[i] ))
-            f3.close()
 
     def Footprint(folder,framecount, step):
-        """Get path of all trajectory files in folder and return the Coordinates for all atoms and different regions."""
+        """This also calculates the distance of all the protein beads from the center of the DNA."""
 
         Trajs = glob.glob(folder+'Traj*.dat')
         Trajs.sort()
@@ -122,6 +68,7 @@ class analyse_traj:
         CXI_frame = []
         for Traj in Trajs:
             """Take XY coordinate for each CA bead."""
+
             CXI_frame.append(analyse_traj.CXYCA(iterator(Traj,framecount,step)))
         Mean_all = []
         STD_all = []
@@ -144,38 +91,39 @@ class analyse_traj:
 
 
     def CountCont(folder, framecount, step, targetC, topo):
-        iterator = Iterator.traj
         """Analyse the formation of contacts to the target side."""    
-        "Get the list of taget side contacts"
+        
+	iterator = Iterator.traj
+	# Get the list of taget side contacts"
         Contacts = pd.read_csv(targetC, sep='\s+', header=None) 
-        """Get the DNA-Positions for each relevant atom form the topology file."""
+        # Get the DNA-Positions for each relevant atom form the topology file.
         P_coord = P.P_coordinates(topo)
-        """Get the relavent coordinates for the contacts you defines."""
+        # Get the relavent coordinates for the contacts you defines.
         iPs = list(Contacts[2])
         P_contacts = P_coord.loc[P_coord['ID'].isin(iPs)]
         Trajs = glob.glob(folder+'Traj*.dat')
         Trajs.sort()
         dfs = []
         for Traj in Trajs:
-            "Analyse each Trajectory individually"
+            # Analyse each Trajectory individually
             CoordinatesTraj= []
             for frame in iterator(Traj,framecount,step):
                 CoordinatesTraj.append(Coordinates.get_coord_traj(frame)) 
-            """Analyse the distances from the dat file."""
+            # Analyse the distances from the dat file.
             fractionformed = []
             Contactformedall = []
             for Frame in CoordinatesTraj:
-                """Iterate over frame and find the atom iR of the repressor,  get the position of the partner P and 
-                calculate the distance. Next evaluate of it is a contact or not."""
+                # Iterate over frame and find the atom iR of the repressor,  get the position of the partner P and 
+                # calculate the distance. Next evaluate of it is a contact or not."""
                 Contactformed = []
                 for R in list(Contacts.index):
-                    iR = int(Contacts[1][R])
-                    iP = int(Contacts[2][R])
+                    iR = int(Contacts[0][R])
+                    iP = int(Contacts[1][R])
                     XYZR = (Frame[0][iR-1],Frame[1][iR-1],Frame[2][iR-1])
                     P_contact = P_contacts.loc[P_contacts['ID'] == iP]
                     XYZP = (float(P_contact['X']),float(P_contact['Y']),float(P_contact['Z']))
                     DistanceRP = geom.dist(XYZR,XYZP)
-                    iMax =int( Contacts[0][R]+3 )
+                    iMax =int( np.sqrt(Contacts[2][R])+3 )
                     if DistanceRP <= iMax:
                         Contactformed.append(1)
                     else:
@@ -219,5 +167,3 @@ folder='{}/'.format(mypath)
 
 analyse_traj.CountCont(folder, 50000, 10, targetC, topo_N)
 #analyse_traj.Footprint(folder,50000,10)
-#analyse_traj.Displacement(folder,50000,10)
-#plot.plot(folder)
